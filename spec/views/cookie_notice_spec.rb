c958ee683ba3faae2472c16a1ac78af8a5bd0263
eadpearce <e.pearce@tutanota.com>
2018-08-08 12:03:32 +0100

    Remove tests that are no longer relevant

--- spec/views/cookie_notice_spec.rb
+++ spec/views/cookie_notice_spec.rb
@@ -13,17 +13,4 @@ RSpec.describe 'export_components/_cookie_notice.html.erb', type: :view do
     @html = Nokogiri::HTML(rendered)
     expect(@html.css('.link')[0]['href']).to eq('https://great.gov.uk/privacy-and-cookies')
   end
-
-  it 'contains a button that dismisses the cookie notice that is correctly labelled' do
-    render
-    @html = Nokogiri::HTML(rendered)
-    expect(@html.css('#dismiss-cookie-notice')[0]['onclick']).to eq('componentsCookieNotice.hideNoticeAndSetCookie()')
-    expect(@html.css('#dismiss-cookie-notice')[0]['aria-label']).to eq('Close this message')
-  end
-
-  it 'contains JS to initially hide the cookie notice' do
-    render
-    @html = Nokogiri::HTML(rendered)
-    expect(@html.css('script')[0].text).to include "$('#header-cookie-notice').hide();"
-  end
 end
