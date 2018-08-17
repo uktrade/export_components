88efc90138de516866e91468d9b3e8e1326640d2
eadpearce <e.pearce@tutanota.com>
2018-08-17 14:28:10 +0100

    Add cookie domain

--- spec/views/cookie_notice_spec.rb
+++ spec/views/cookie_notice_spec.rb
@@ -14,3 +14,21 @@ RSpec.describe 'export_components/_cookie_notice.html.erb', type: :view do
     expect(@html.css('.link')[0]['href']).to eq('https://great.gov.uk/privacy-and-cookies')
   end
 end
+
+RSpec.describe 'export_components/_cookie_domain_meta.html.erb', type: :view do
+  it 'renders env var' do
+    stub_const('Figaro', double)
+    allow(Figaro).to receive_message_chain(:env, :PRIVACY_COOKIE_DOMAIN).and_return('.test.gov.uk')
+    render
+    @html = Nokogiri::HTML(rendered)
+    expect(@html.css('meta')[0]['value']).to eq('.test.gov.uk')
+  end
+
+  it 'renders default value' do
+    stub_const('Figaro', double)
+    allow(Figaro).to receive_message_chain(:env, :PRIVACY_COOKIE_DOMAIN).and_return(nil)
+    render
+    @html = Nokogiri::HTML(rendered)
+    expect(@html.css('meta')[0]['value']).to eq('.great.gov.uk')
+  end
+end
